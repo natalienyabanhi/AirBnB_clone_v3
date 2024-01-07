1b78b7c59da61122708ada4ee92ddf071f012ddf
Natalie Nyabanhi <122810697+natalienyabanhi@users.noreply.github.com>
2024-01-07 23:12:47 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,41 +1,28 @@
 #!/usr/bin/python3
-"""Flask web server to handle API requests.
-
-This module sets up a Flask web server that handles API requests.
-It registers the necessary routes and configurations
-to respond to HTTP requests.
-The server listens on the specified host and port,
-which can be customized through environment variables.
-"""
-
-from api.v1.views import app_views
-from flask import Blueprint, Flask, jsonify
-from flask_cors import CORS
-from models import storage
+'''Flask web application API
+'''
 from os import getenv
+from flask import Flask, make_response, jsonify
+from models import storage
+from api.v1.views import app_views
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
-
-CORS(app, origins="0.0.0.0")
-
-api_host = getenv("HBNB_API_HOST", "0.0.0.0")
-api_port = getenv("HBNB_API_PORT", 5000)
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """Closes the database storage connection."""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """Handles 404 errors and returns a JSON-formatted 404 response."""
-    return jsonify({"error": "Not found"}), 404
+def page_not_found(e):
+    return {"error": "Not found"}, 404
 
 
-if __name__ == "__main__":
-    app.run(host=api_host, port=int(api_port), threaded=True)
+host = getenv('HBNB_API_HOST', '0.0.0.0')
+port = getenv('HBNB_API_PORT', 5000)
+if __name__ == '__main__':
+    app.run(host=host, port=port, threaded=True)
